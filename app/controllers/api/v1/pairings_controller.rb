7f8a7151996178c231d60b388ff36f4d5487bca7
Joe Letner <jrletner@gmail.com>
2023-06-03 22:29:59 -0500

    pairings route

--- app/controllers/api/v1/pairings_controller.rb
+++ app/controllers/api/v1/pairings_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class PairingsController < Api::V1::ApplicationController
+      def index
+        pairing = Pairing.all
+        render json: pairing
+      end
     end
   end
 end
